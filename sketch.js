18056861bace3b8f23f2525f0b109ebd485105b1
Maicon Silva <118707919+linseu@users.noreply.github.com>
2024-05-14 20:17:44 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,15 +1,16 @@
 function setup() {
-    createCanvas(600, 400);
-    background("rgb(12,12,145)")
-  }
+  createCanvas(600, 600);
+  background("rgb(12,12,145)")
+}
+
+function draw() {
+  stroke("yellow");
+  fill("green");
   
-  function draw() {
-    stroke("yellow");
-    fill("green");
-    
-    
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
+}
+
   
